48e666402d0319f13d99f2d6cff8e93ed42f635b
Hakurei Reimu <24761242+Hakurei-Reimu-SkylarkStudio@users.noreply.github.com>
2018-07-25 16:45:05 +0800

    201807251645

--- MNISTMult.py
+++ MNISTMult.py
@@ -1,7 +1,20 @@
 #MNISTMult.py
+#Can't run in Tf 1.9
 
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+sess = tf.Session()
+#sess = tf.InteractiveSession()
+x = tf.placeholder("float", shape=[None, 784])
+y_ = tf.placeholder("float", shape=[None, 10])
+# W = tf.Variable(tf.zeros([784, 10]))
+# b = tf.Variable(tf.zeros([10]))
+# sess.run(tf.initialize_all_variables())
+# y = tf.nn.softmax(tf.matmul(x, W) + b)
+# cross_entropy = -tf.reduce_sum(y_*tf.log(y))
+# train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
 
 
 def weight_variable(shape):
@@ -19,18 +32,17 @@ def conv2d(x, W):
 
 
 def max_pool_2x2(x):
-  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-  strides=[1, 2, 2, 1], padding='SAME')
+  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
-mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
-x = tf.placeholder("float", shape=[None, 784])
-y_ = tf.placeholder("float", shape=[None, 10])
-sess = tf.Session()
+
 W_conv1 = weight_variable([5, 5, 1, 32])
 b_conv1 = bias_variable([32])
+
 x_image = tf.reshape(x, [-1, 28, 28, 1])
+
 h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1)
+
 W_conv2 = weight_variable([5, 5, 32, 64])
 b_conv2 = bias_variable([64])
 
@@ -41,8 +53,10 @@ b_fc1 = bias_variable([1024])
 
 h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
 keep_prob = tf.placeholder("float")
 h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
 W_fc2 = weight_variable([1024, 10])
 b_fc2 = bias_variable([10])
 
@@ -51,7 +65,7 @@ cross_entropy = -tf.reduce_sum(y_*tf.log(y_conv))
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-sess.run(tf.initialize_all_variables())
+sess.run(tf.global_variables_initializer())
 for i in range(20000):
   batch = mnist.train.next_batch(50)
   if i % 100 == 0:
@@ -60,5 +74,5 @@ for i in range(20000):
     print("step %d, training accuracy %g" % (i, train_accuracy))
   train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
-print("test accuracy %g" % accuracy.eval(feed_dict={
-    x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+print("test accuracy %g" % accuracy.eval(
+    feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
--- MNISTSingle.py
+++ MNISTSingle.py
@@ -4,9 +4,9 @@ import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-x = tf.placeholder("float", [None, 784])
 W = tf.Variable(tf.zeros([784,10]))
 b = tf.Variable(tf.zeros([10]))
+x = tf.placeholder("float", [None, 784])
 y = tf.nn.softmax(tf.matmul(x,W) + b)
 y_ = tf.placeholder("float", [None,10])
 cross_entropy = -tf.reduce_sum(y_*tf.log(y))
