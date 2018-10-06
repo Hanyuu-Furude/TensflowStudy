235208f9f59d711a51a97c54ea7fa2a5e933331a
Hanyuu <HanyuuFurude@outlook.com>
2018-10-06 12:19:02 +0800

    201810061218

--- studyNotes/OpenCV/OpenCVBasicOperations.py
+++ studyNotes/OpenCV/OpenCVBasicOperations.py
@@ -2,6 +2,10 @@ import cv2
 
 img = cv2.imread('example.jpg')
 print('img[300,300]=%s' % img[300,300])
+print('blue pixel %s' %img[300,300,0])
+print('green pixel %s' %img[300,300,1])
+print('red pixel %s' %img[300,300,2])
+print(img[300,300,3])
 
 cv2.imshow('img', img)
 k = cv2.waitKey(0)  # & 0xff
@@ -9,3 +13,4 @@ if k == 27:  # ESC
 	cv2.destroyAllWindows()
 elif k == ord('s'):
 	cv2.imwrite('Save.jpg', img)
+ 
