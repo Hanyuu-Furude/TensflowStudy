b1cdca913dc859f4e96380520d56f0443827d0a0
Hanyuu Furude <HanyuuFurude@outlook.com>
2018-12-20 16:23:05 +0800

    201812201623

---
+++ studyNotes/Others/seu_wlan/test.py
@@ -0,0 +1,11 @@
+import time
+with open('a.txt', 'a') as cc:
+    # pass
+    # while True:
+        # try:
+    for i in range(8):
+            time.sleep(1)
+            cc.writelines('a\n')
+            print('a')
+        # except:
+        #     pass
